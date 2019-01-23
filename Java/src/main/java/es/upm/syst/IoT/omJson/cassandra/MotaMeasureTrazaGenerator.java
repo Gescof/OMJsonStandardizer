87c7964351ab86d856c799624d6001f245b5fb69
Gescof <gescolarfernandez@gmail.com>
2019-01-23 18:05:19 +0100

    Agregadas clases de configuración con Cassandra y librerías.

---
+++ Java/src/main/java/es/upm/syst/IoT/omJson/cassandra/MotaMeasureTrazaGenerator.java
@@ -0,0 +1,9 @@
+package es.upm.syst.IoT.omJson.cassandra;
+
+public class MotaMeasureTrazaGenerator {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
---
+++ Java/src/main/java/es/upm/syst/IoT/omJson/cassandra/OMJsonGenerator.java
@@ -0,0 +1,9 @@
+package es.upm.syst.IoT.omJson.cassandra;
+
+public class OMJsonGenerator {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
